ea5c0f7d0ffdd931f8bdfb7a533f6b93f26a4944
eshban14 <eshban.younis@gmail.com>
2024-07-27 14:27:58 +0400

    first commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+
+function menuClick(){
+    document.getElementsByClassName('MobileNavMenu')[0].classList.toggle('MobileNavMenuDeactivate')
+    
+
+    document.getElementsByClassName('fa-times')[0].classList.toggle('xicon')
+    document.getElementsByClassName('fa-bars')[0].classList.toggle('icon')
+}
+
+
